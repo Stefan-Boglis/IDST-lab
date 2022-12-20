dc51208916da5c975480a97033913e870ecd1258
Stefan Boglis <stefan.boglis1@gmail.com>
2022-12-20 21:25:39 +0200

    token from CLI argument

--- idst_lab3/laborator.py
+++ idst_lab3/laborator.py
@@ -1,4 +1,11 @@
-#am uitat sa dau commit atunci cand am adaugat aici copyright notice asa ca bag acum ceva sa am la ce sa dau commit
+
+#Copyright (C) 2022 Stefan Boglis
+
+   #This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+   #This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+   # You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
 #!./.venv/bin/python
 import discord      # base discord module
 import code         # code.interact
@@ -6,6 +13,7 @@ import os           # environment variables
 import inspect      # call stack inspection
 import random       # dumb random number generator
 from discord.ext import commands    # Bot class and utils
+import argparse
 ################################################################################
 ############################### HELPER FUNCTIONS ###############################
 ################################################################################
@@ -93,6 +101,13 @@ if __name__ == '__main__':
     if 'BOT_TOKEN' not in os.environ:
         log_msg('save your token in the BOT_TOKEN env variable!', 'error')
         exit(-1)
-    # launch bot (blocking operation)
-    bot.run(os.environ['BOT_TOKEN'])
+    # get token from argument
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--token", help="input bot token") 
+    args = parser.parse_args()
+    if args.token:
+        bot.run(args.token)
+    else:
+        # launch bot (blocking operation)   
+        bot.run(os.environ['BOT_TOKEN'])
     # code.interact(local=dict(globals(), **locals())) 
